21f26f8763bf6a30c5835aa66dbb4b735d4c6fab
Daria Balzhy <dbalzhy@gmail.com>
2024-04-11 11:56:55 -0400

    carousel

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ const container = document.querySelector('.container')
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+
+/** carousel block */
+let slideIndex = 1
+
+function showSlides(n) {
+    let slides = document.getElementById('imgs').getElementsByTagName('img')
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+}
+
+let interval = setInterval(function () {
+    slideIndex++;
+    showSlides(slideIndex);
+}, 3000);
+
+
+
